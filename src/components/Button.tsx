4e3a7b32bcdfc2439bde0b22f462fa25a73ee29c
DAPSY-dev <bboydapsy@gmail.com>
2026-09-25 06:48:22 +0300

    feat(styles): base button architecture

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,42 @@
 import type { ComponentProps } from 'react';
+import { Icon } from './Icon';
 
-export type ButtonProps = ComponentProps<'button'>;
+export type ButtonProps = ComponentProps<'button'> & {
+  variant?: 'primary' | 'secondary' | 'tertiary';
+  size?: 'xs' | 's' | 'm' | 'l' | 'xl';
+  loading?: boolean;
+};
 
-export function Button({ type = 'button', className, ...props }: ButtonProps) {
+export function Button({
+  type = 'button',
+  className,
+  variant,
+  size,
+  loading = false,
+  children,
+  ...props
+}: ButtonProps) {
   return (
     <button
       {...props}
       type={type}
-      className={['button', className].filter(Boolean).join(' ')}
-    />
+      className={[
+        'button',
+        variant && `button--${variant}`,
+        loading && 'button--loading',
+        className,
+      ]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      {loading ? (
+        <>
+          <Icon name="preloader" />
+          <span className="visually-hidden">Loading…</span>
+        </>
+      ) : (
+        children
+      )}
+    </button>
   );
 }
--- src/pages/UiButtonsPage.tsx
+++ src/pages/UiButtonsPage.tsx
@@ -2,7 +2,6 @@ import { AuthModal } from '../features/auth/components/AuthModal';
 import { Layout } from '../components/Layout';
 import { Link } from 'react-router';
 import { Button } from '../components/Button';
-import { Icon } from '../components/Icon';
 
 export function UiButtonsPage() {
   return (
@@ -10,11 +9,59 @@ export function UiButtonsPage() {
       <Layout>
         <Link to="/ui">Back to UI showcase</Link>
         <h1>Buttons</h1>
-        <Button>Example button</Button>
-        <Button disabled>Disabled button</Button>
-        <Button>
-          <Icon name="close" /> Button with icon
-        </Button>
+        <h2>Variants</h2>
+        <ul>
+          <li>
+            <Button>Button</Button>
+          </li>
+          <li>
+            <Button variant="primary">Button</Button>
+          </li>
+          <li>
+            <Button variant="secondary">Button</Button>
+          </li>
+          <li>
+            <Button variant="tertiary">Button</Button>
+          </li>
+          <li>
+            <Button variant="primary" loading>
+              Button
+            </Button>
+          </li>
+          <li>
+            <Button variant="primary" disabled>
+              Button
+            </Button>
+          </li>
+        </ul>
+        <h2>Sizes</h2>
+        <ul>
+          <li>
+            <Button variant="primary" size="xs">
+              Button
+            </Button>
+          </li>
+          <li>
+            <Button variant="primary" size="s">
+              Button
+            </Button>
+          </li>
+          <li>
+            <Button variant="primary" size="m">
+              Button
+            </Button>
+          </li>
+          <li>
+            <Button variant="primary" size="l">
+              Button
+            </Button>
+          </li>
+          <li>
+            <Button variant="primary" size="xl">
+              Button
+            </Button>
+          </li>
+        </ul>
       </Layout>
       <AuthModal />
     </>
